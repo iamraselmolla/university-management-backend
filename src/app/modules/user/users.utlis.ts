dcbba5f32b37e36e5127072d05c06aa7c0757046
iamraselmolla <raselmolla6336@gmail.com>
2024-06-14 23:56:36 +0600

    added function for creating unique student id

--- src/app/modules/user/users.utlis.ts
+++ src/app/modules/user/users.utlis.ts
@@ -1,13 +1,15 @@
+import { IAcademicSemester } from "../academicSemester/academicSemester.interface";
 import { User } from "./users.model";
 
-const getlastUserId = async () => {
+const findLastStudentUserId = async () => {
   const userId = await User.findOne({}, { id: 1, _id: 0 }).sort({ createdAt: -1 }).lean();
   return userId?.id
 }
-const generateUserId = async () => {
-  const LastUserId = getlastUserId()
+const generateStudentId = async (academicSemester: IAcademicSemester) => {
+  const LastUserId = findLastStudentUserId()
   const currentId = await LastUserId || String(0).padStart(5, "0");
-  const incrementId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  let incrementId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+  incrementId = `${academicSemester.year.substring(2)}${academicSemester.code}${incrementId}`;
   return incrementId;
 }
-export default generateUserId;
+export default generateStudentId;
